d0fd4a566823f774269c4dc4057df1f004d2e743
mendezjavier <53797004+mendezjavier@users.noreply.github.com>
2019-11-01 11:13:05 -0300

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -5,14 +5,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 process.env.CADUCIDAD_TOKEN = '48h';
 
 // CONTRASENA DEL TOKEN
-process.env.SEED = process.env.SEED || 'P@rk1ng-@PP-TOKEN';
+process.env.SEED = process.env.SEED || 'otro';
 
 // ID PARA AUTH CON GOOGLE
-process.env.CLIENT_ID = process.env.CLIENT_ID || '762295368431-gb104gc5h1n6g6g012qfpangb13dvcg5.apps.googleusercontent.com';
+process.env.CLIENT_ID = process.env.CLIENT_ID || 'id';
 // BASE DE DATOS
 let urlDB;
 if (process.env.NODE_ENV == 'dev') {
-    urlDB = 'mongodb://10.245.107.135:27017/canchas-app';
     urlDB = 'mongodb://localhost:27017/canchas-app';
 } else {
     urlDB = process.env.MONGO_URI;
